07450a23c25555d7436f39d63c1c4d5d9f0a323a
pulchra <155280651+pullchra@users.noreply.github.com>
2025-03-04 23:58:49 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -Command "Start-Process -FilePath 'powershell.exe' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0AntGrab.ps1\"' -Verb RunAs"
+powershell -Command "Start-Process -FilePath 'powershell.exe' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0AntWare.ps1\"' -Verb RunAs"
